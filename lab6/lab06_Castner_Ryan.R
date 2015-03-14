0311182350dc57e07f14d30db165d032ac0f0871
Ryan Castner <castner.rr@gmail.com>
2015-03-13 23:39:58 -0400

    added ggplot functions

--- lab6/lab06_Castner_Ryan.R
+++ lab6/lab06_Castner_Ryan.R
@@ -27,8 +27,50 @@ someWrapperFunction <- function(){
   return(avgs)
 }
 
-df <- someWrapperFunction()
+#df <- someWrapperFunction()
+ggBox <- function(df){
+  g <- ggplot(df) + geom_boxplot(aes(x=1, y=df[,1], fill="A")) +
+            geom_boxplot(aes(x=2, y=df[,2], fill="B")) + geom_boxplot(aes(x=3, y=df[,3], fill="C")) + 
+            geom_boxplot(aes(x=4, y=df[,4], fill="D")) + geom_boxplot(aes(x=5,y=df[,5], fill="E")) + 
+            scale_colour_manual(breaks=c("A","B","C","D","E"),
+                                labels=c("A","B","C","D","E"),
+                                values=c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2"))
+  g <- g+ggtitle('Hela Data')+theme(plot.title = element_text(size=20, face="bold",vjust=2))
+  g <- g+labs(x="Groups", y="Average Distribution")
+  g <- g+scale_x_continuous(label=function(x){return(LETTERS[x])})
+  g <- g+theme(legend.title=element_blank(), legend.key=element_rect(fill=NA))
+  ggsave(file="boxplot-hela.png")
+  
+  print(g)
+}
 
-ggbox <- ggplot(df) + geom_boxplot(aes(x=1,y=df[,1]))
-          + geom_boxplot(aes(x=1,y=df[,2])) + geom_boxplot(aes(x=1,y=df[,3])) + geom_boxplot(df[,4])
-          + geom_boxplot(aes(x=1,y=df[,5]))
+ggDense <- function(df){
+  g <- ggplot(df) + geom_density(aes(x=df[,1], fill="A"), alpha=.5) +
+    geom_density(aes(x=df[,2], fill="B"), alpha=.5) +
+    geom_density(aes(x=df[,3], fill="C"), alpha=.5) +
+    geom_density(aes(x=df[,4], fill="D"), alpha=.5) +
+    geom_density(aes(x=df[,5], fill="E"), alpha=.5) +
+    scale_colour_manual(breaks=c("A","B","C","D","E"),
+                        labels=c("A","B","C","D","E"),
+                        values=c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2")) +
+    ggtitle('Hela Data Density Plot')+theme(plot.title = element_text(size=20, face="bold", vjust=2)) +
+    labs(x="Mean values",y="Density") + theme(legend.title=element_blank(), legend.key=element_rect(fill=NA))
+  ggsave(file="densityplot-hela.png")
+  print(g)
+}
+
+ggStat <- function(df){
+  g <- ggplot(df) + geom_jitter(aes(x=df[,1],y=1.0,colour="A",group=1)) +
+    geom_jitter(aes(x=df[,2],y=1.0,colour="B",group=1)) +
+  geom_jitter(aes(x=df[,3],y=1.0,colour="C",group=1)) +
+  geom_jitter(aes(x=df[,4],y=1.0,colour="D",group=1)) +
+  geom_jitter(aes(x=df[,5],y=1.0,colour="E",group=1)) +
+  scale_colour_manual(breaks=c("A","B","C","D","E"),
+                      labels=c("A","B","C","D","E"),
+                      values=c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2")) +
+    ggtitle('Hela Data Scatterplot')+theme(plot.title = element_text(size=20, face="bold", vjust=2)) +
+    labs(x="Mean values",y="Display") + theme(legend.title=element_blank(), legend.key=element_rect(fill=NA)) +
+  geom_smooth(method="gam", aes(x=(group=1),y=1)) + stat_smooth(method="gam", aes(x=(group=1),y=1))
+  ggsave(file="scatterplot-hela.png")
+  print(g)
+}
